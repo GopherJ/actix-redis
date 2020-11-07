a058eb64f63178348ad9891a5cecd365ad605c54
Cheng JIANG <jiang.cheng@vip.163.com>
2020-11-07 19:18:06 +0100

    add subscribe & psubscribe support

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,24 @@
+use std::net::SocketAddr;
+
 use actix::{
     fut::{err, ok},
     prelude::*,
 };
 use backoff::{backoff::Backoff, ExponentialBackoff};
-use log::{error, info, warn};
-
 use bb8_redis::{
     bb8,
     redis::{cmd, ErrorKind, RedisError, RedisResult, ToRedisArgs, Value as RedisValue},
     RedisConnectionManager, RedisPool,
 };
+use log::{error, info, warn};
+use redis_async::{
+    client::pubsub::PubsubStream,
+    client::{pubsub_connect, PubsubConnection},
+    error::Error,
+    resp::RespValue,
+};
+
+pub type StreamHandlerFn = fn(item: Result<RespValue, Error>);
 
 pub enum RedisCmd<T: ToRedisArgs> {
     Set(String, T),
@@ -38,22 +47,37 @@ impl<T: ToRedisArgs + Unpin + 'static> Message for RedisCmd<T> {
 }
 
 pub struct RedisClient {
-    addr: String,
+    url: String,
+    addr: SocketAddr,
     backoff: ExponentialBackoff,
     pool: Option<RedisPool>,
+    pubsub: Option<PubsubConnection>,
+    sub_topics: Option<Vec<String>>,
+    psub_topics: Option<Vec<String>>,
+    stream_handler_fn: Option<StreamHandlerFn>,
 }
 
 impl RedisClient {
-    pub fn start<S: Into<String>>(addr: S) -> Addr<RedisClient> {
-        let addr = addr.into();
+    pub fn start<S: Into<String>>(
+        url: S,
+        sub_topics: Option<Vec<String>>,
+        psub_topics: Option<Vec<String>>,
+        stream_handler_fn: Option<StreamHandlerFn>,
+    ) -> Addr<RedisClient> {
+        let url = url.into();
 
         let mut backoff = ExponentialBackoff::default();
         backoff.max_elapsed_time = None;
 
-        Supervisor::start(|_| RedisClient {
-            addr,
+        Supervisor::start(move |_| RedisClient {
+            addr: url.parse().unwrap(),
+            url,
             backoff,
             pool: None,
+            pubsub: None,
+            sub_topics,
+            psub_topics,
+            stream_handler_fn,
         })
     }
 }
@@ -62,25 +86,76 @@ impl Actor for RedisClient {
     type Context = Context<Self>;
 
     fn started(&mut self, ctx: &mut Context<Self>) {
-        match RedisConnectionManager::new(self.addr.as_str()) {
+        match RedisConnectionManager::new(self.url.as_str()) {
             Ok(manager) => {
                 bb8::Pool::builder()
                     .build(manager)
                     .into_actor(self)
-                    .map(|res, act, ctx| match res {
+                    .map(|res, act, _ctx| match res {
                         Ok(pool) => {
-                            info!("Connected to redis_client server: {}", act.addr);
+                            info!("Created connection pool to redis server: {}", act.url);
                             act.pool = Some(RedisPool::new(pool));
-                            act.backoff.reset();
                         }
                         Err(err) => {
                             error!("Cannot build bb8 redis_client pool: {}", err);
-                            if let Some(timeout) = act.backoff.next_backoff() {
-                                ctx.run_later(timeout, |_, ctx| ctx.stop());
-                            }
                         }
                     })
                     .wait(ctx);
+
+                let addr = self.addr.clone();
+                let sub_topics = self.sub_topics.clone().unwrap_or(vec![]);
+                let psub_topics = self.psub_topics.clone().unwrap_or(vec![]);
+
+                async move {
+                    let pubsub_conn = pubsub_connect(&addr).await?;
+                    let mut sub_streams = vec![];
+                    let mut psub_streams = vec![];
+
+                    for sub_topic in sub_topics {
+                        sub_streams.push(pubsub_conn.subscribe(&sub_topic).await?);
+                    }
+
+                    for psub_topic in psub_topics {
+                        psub_streams.push(pubsub_conn.psubscribe(&psub_topic).await?);
+                    }
+
+                    Ok((pubsub_conn, sub_streams, psub_streams))
+                }
+                .into_actor(self)
+                .map(
+                    |res: Result<
+                        (PubsubConnection, Vec<PubsubStream>, Vec<PubsubStream>),
+                        Error,
+                    >,
+                     act,
+                     ctx| match res {
+                        Ok((pubsub_conn, sub_streams, psub_streams)) => {
+                            info!("Created pubsub connection to redis server: {}", act.url);
+
+                            for sub_stream in sub_streams {
+                                ctx.add_stream(sub_stream);
+                            }
+
+                            for psub_stream in psub_streams {
+                                ctx.add_stream(psub_stream);
+                            }
+
+                            act.pubsub = Some(pubsub_conn);
+                        }
+                        Err(err) => {
+                            error!("Cannot build bb8 redis_client pool: {}", err);
+                        }
+                    },
+                )
+                .wait(ctx);
+
+                if self.pool.is_some() && self.pubsub.is_some() {
+                    self.backoff.reset();
+                } else {
+                    if let Some(timeout) = self.backoff.next_backoff() {
+                        ctx.run_later(timeout, |_, ctx| ctx.stop());
+                    }
+                }
             }
             Err(err) => {
                 error!("Cannot build bb8 redis_client manager: {}", err);
@@ -95,6 +170,19 @@ impl Actor for RedisClient {
 impl Supervised for RedisClient {
     fn restarting(&mut self, _: &mut Self::Context) {
         self.pool.take();
+        self.pubsub.take();
+    }
+}
+
+impl StreamHandler<Result<RespValue, Error>> for RedisClient {
+    fn handle(&mut self, item: Result<RespValue, Error>, _ctx: &mut Context<RedisClient>) {
+        if let Some(stream_handler_fn) = self.stream_handler_fn {
+            stream_handler_fn(item)
+        }
+    }
+
+    fn finished(&mut self, ctx: &mut Self::Context) {
+        ctx.stop()
     }
 }
 
@@ -204,7 +292,7 @@ impl<T: ToRedisArgs + Unpin + 'static> Handler<RedisCmd<T>> for RedisClient {
                 Ok(res_ok) => ok(res_ok),
                 Err(res_err) => {
                     if res_err.is_io_error() {
-                        warn!("Redis connection error: {} error: {}", act.addr, res_err);
+                        warn!("Redis connection error: {} error: {}", act.url, res_err);
                         ctx.stop();
                     }
                     err(res_err)
